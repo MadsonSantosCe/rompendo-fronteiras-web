215a4f30da30e4dc7227a44525ffb7ebcb73e641
Madson Santos <madsonsamsung@gmail.com>
2025-05-01 13:13:19 -0300

    Refactor theme management and add mode toggle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './App.css'
+import "./App.css";
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
-        Hello World
+        <ModeToggle />
       </div>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
